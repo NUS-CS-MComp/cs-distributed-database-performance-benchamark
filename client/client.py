e9218ad120b1c2923e9d6662f472aa349970ffb6
Terry Lu <25686506+terryluzj@users.noreply.github.com>
2020-10-18 22:05:53 +0800

    [Common] Basic Python project setup

--- client/client.py
+++ client/client.py
@@ -2,10 +2,14 @@ import argparse
 
 
 def parse():
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--load', help='path to the data to be loaded into the db')
-    parser.add_argument('--run', help='path to the transaction file to be executed')
-    parser.add_argument('--purge', help='purge the database contents')
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument(
+        "--load", help="path to the data to be loaded into the db"
+    )
+    parser.add_argument(
+        "--run", help="path to the transaction file to be executed"
+    )
+    parser.add_argument("--purge", help="purge the database contents")
 
     args = parser.parse_args()
     return args
