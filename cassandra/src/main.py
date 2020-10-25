33b42deaa3e827058179d6e9d98a280c36f425a1
cs4224o <cs4224o@xcnc35.comp.nus.edu.sg>
2020-10-25 13:57:17 +0800

    [Cassandra] present payment amount in cent

--- cassandra/src/main.py
+++ cassandra/src/main.py
@@ -8,13 +8,13 @@ if __name__ == '__main__':
     cluster = Cluster(['127.0.0.1'], port=6042)
     session = cluster.connect('cs5424')
 
-    # item_number = [68195, 26567]
-    # supplier_warehouse = [1, 1]
-    # quantity = [1, 5]
-    # output = new_order(session, 1, 1, 1279, 2, item_number, supplier_warehouse, quantity)
-    # print(output)
+    item_number = [68195, 26567]
+    supplier_warehouse = [1, 1]
+    quantity = [1, 5]
+    output = new_order(session, 1, 1, 1279, 2, item_number, supplier_warehouse, quantity)
+    print(output)
 
-    # output = payment(session, 1, 2, 186, 22378)
-    # print(output)
+    output = payment(session, 1, 2, 186, 22378)
+    print(output)
 
     output = delivery(session, 1, 7)
