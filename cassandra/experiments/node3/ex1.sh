ab116da57b84f55bb42036b6835efd9aca45dff5
cs4224o <cs4224o@xcnc35.comp.nus.edu.sg>
2020-10-31 17:29:04 +0800

    [Cassandra] test experiment

---
+++ cassandra/experiments/node3/ex1.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+SCRIPT_PATH="/home/stuproj/cs4224o/cs5424-distributed-database-group-project/cassandra/src/main.py"
+XACT_FILE_DIR="/home/stuproj/cs4224o/cs5424-distributed-database-group-project/cassandra/project-files/xact-files/"
+
+for i in {3..20..5}
+do
+    python3 ${SCRIPT_PATH} < ${XACT_FILE_DIR}$i.txt &
+done
--- cassandra/src/main.py
+++ cassandra/src/main.py
@@ -1,3 +1,7 @@
+import sys
+import time
+import pandas as pd
+from decimal import Decimal
 from cassandra.cluster import Cluster
 from transactions.new_order import *
 from transactions.payment import *
@@ -9,16 +13,60 @@ if __name__ == '__main__':
     cluster = Cluster(['127.0.0.1'], port=6042)
     session = cluster.connect('cs5424')
 
-    # item_number = [68195, 26567]
-    # supplier_warehouse = [1, 1]
-    # quantity = [1, 5]
-    # output = new_order(session, 1, 1, 1279, 2, item_number, supplier_warehouse, quantity)
-    # print(output)
+    input_data = sys.stdin.readlines()
+    nlines = len(input_data)
 
-    # output = payment(session, 1, 2, 186, 22378)
-    # print(output)
-
-    output = delivery(session, 1, 7)
-
-    output = order_status(session, 1, 2, 1081)
-    print(output)
+    latency = []
+    nTX = 0
+    l = 0
+    # while (l < nlines):
+    while (l < 10):
+        req = input_data[l]
+        args = req.split(",")
+        start_time = time.time()
+        if args[0] == "N":
+            c_id = int(args[1])
+            w_id = int(args[2])
+            d_id = int(args[3])
+            m = int(args[4])
+            item_number, supplier_warehouse, quantity = [], [], []
+            for i in range(m):
+                data = input_data[l+i+1]
+                args = data.split(",")
+                item_number.append(int(args[0]))
+                supplier_warehouse.append(int(args[1]))
+                quantity.append(int(args[2]))
+            output = new_order(session, w_id, d_id, c_id, m, item_number, supplier_warehouse, quantity)
+            print(output)
+            l += m
+        elif args[0] == "P":
+            c_w_id = int(args[1])
+            c_d_id = int(args[2])
+            c_id = int(args[3])
+            amount = Decimal(args[4])
+            output = payment(session, c_w_id, c_d_id, c_id, amount)
+            print(output)
+        elif args[0] == "D":
+            w_id = int(args[1])
+            carrier_id = int(args[2])
+            delivery(session, w_id, carrier_id)
+        elif args[0] == "O":
+            c_w_id = int(args[1])
+            c_d_id = int(args[2])
+            c_id = int(args[3])
+            output = order_status(session, c_w_id, c_d_id, c_id)
+            print(output)
+        latency.append(time.time() - start_time)
+        nTX += 1
+        l += 1
+    
+    df = pd.Series(latency)
+    result = {}
+    result['nTXs'] = nTX
+    result['total'] = df.sum()
+    result['throughput'] = nTX / result['total']
+    result['average'] = df.mean()
+    result['median'] = df.median()
+    result['95percentile'] = df.quantile(0.95)
+    result['99percentile'] = df.quantile(0.99)
+    print(result)
