c9ffc7be15acd1c41be7aaa870c33a0a99e9e000
Zach <>
2020-11-04 02:10:00 +0800

    fix caps

--- cassandra/src/transactions/popular_item.py
+++ cassandra/src/transactions/popular_item.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from transactions.utils import *
 
 
 def popular_item(session, warehouse, district, limit):
@@ -12,10 +13,10 @@ def popular_item(session, warehouse, district, limit):
     cql_get_customer_names = 'SELECT C_ID, C_FIRST, C_MIDDLE, C_LAST FROM customer ' \
                              'WHERE C_W_ID = {w} AND C_D_ID = {d} AND C_ID IN {c}'
     orders = session.execute(cql_get_orders.format(w=warehouse, d=district, l=limit))
-    order_ids = [order.O_ID for order in orders]
-    customer_ids = [order.O_C_ID for order in orders]
-    ols = session.execute(cql_get_ols.format(w=warehouse, d=district, o=tuple(order_ids)))
-    customers = session.execute(cql_get_customer_names.format(w=warehouse, d=district, c=tuple(customer_ids)))
+    order_ids = [order.o_id for order in orders]
+    customer_ids = [order.o_c_id for order in orders]
+    ols = session.execute(cql_get_ols.format(w=warehouse, d=district, o=get_tuple(tuple(order_ids))))
+    customers = session.execute(cql_get_customer_names.format(w=warehouse, d=district, c=get_tuple(tuple(customer_ids))))
 
     all_popular_item_ids = set()
     ols_by_order = defaultdict(list)
@@ -23,36 +24,36 @@ def popular_item(session, warehouse, district, limit):
     customers_by_id = defaultdict(list)
     item_occurance = defaultdict(int)
     for ol in ols:
-        ols_by_order[ol.OL_O_ID].append(ol)
+        ols_by_order[ol.ol_o_id].append(ol)
     for customer in customers:
-        customers_by_id[customer.C_ID].append(customer)
+        customers_by_id[customer.c_id].append(customer)
     for id in order_ids:
-        max_quantity = max([ol.OL_QUANTITY for ol in ols_by_order[id]])
-        popular_items_in_order = [ol.OL_I_ID for ol in ols_by_order[id] if ol.OL_QUANTITY == max_quantity]
+        max_quantity = max([ol.ol_quantity for ol in ols_by_order[id]])
+        popular_items_in_order = [ol.ol_i_id for ol in ols_by_order[id] if ol.OL_QUANTITY == max_quantity]
         popular_items_by_order[id] = (popular_items_in_order, max_quantity)
         all_popular_item_ids.update(popular_items_in_order)
         for id in popular_items_in_order:
             item_occurance[id] += 1
 
-    items = [item for item in session.execute(cql_get_items.format(i=tuple(all_popular_item_ids)))]
+    items = [item for item in session.execute(cql_get_items.format(i=get_tuple(tuple(all_popular_item_ids))))]
     item_by_id = {}
     for item in items:
-        item_by_id[item.I_ID] = item
+        item_by_id[item.i_id] = item
 
     result = {'identifier': (warehouse, district), 'number': limit, 'orders': []}
     for order in orders:
-        customer = customers_by_id[order.O_C_ID]
-        entry = {'O_ID': order.O_ID,
-                 'O_ENTRY_D': order.O_ENTRY_D,
-                 'C_FIRST': customer.C_FIRST,
-                 'C_MIDDLE': customer.C_MIDDLE,
-                 'C_LAST': customer.C_LAST,
-                 'popular_items': [{'name': item_by_id[item_id].I_NAME, 'quantity': max}
-                                    for ids, max in popular_items_by_order[order.O_ID]
+        customer = customers_by_id[order.o_c_id]
+        entry = {'O_ID': order.o_id,
+                 'O_ENTRY_D': order.o_entry_d,
+                 'C_FIRST': customer.c_first,
+                 'C_MIDDLE': customer.c_middle,
+                 'C_LAST': customer.c_last,
+                 'popular_items': [{'name': item_by_id[item_id].i_name, 'quantity': max}
+                                    for ids, max in popular_items_by_order[order.o_id]
                                     for item_id in ids]}
         result['orders'].append(entry)
     for id in all_popular_item_ids:
-        entry = {'name': item_by_id[id].I_NAME,
+        entry = {'name': item_by_id[id].i_name,
                  'percentage': item_occurance[id] * 1.0 / limit}
         result['popular_items'].append(entry)
     return result
