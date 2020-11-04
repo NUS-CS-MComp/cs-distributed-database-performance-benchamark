12f51ecf3ab9cc52e85c75496ad95e67b8d8997a
zengxi2729 <zengxi2729@gmail.com>
2020-11-04 22:38:40 +0800

    update stock level sql

--- cockroachdb/scripts/db/transactions/stock_level.sql
+++ cockroachdb/scripts/db/transactions/stock_level.sql
@@ -15,3 +15,16 @@ JOIN (
     WHERE ol_w_id = 1 AND ol_d_id = 1
 ) ol ON s_w_id = ol.ol_w_id AND s_i_id = ol.ol_i_id
 WHERE s_quantity < 1000;
+
+
+SELECT COUNT((s_w_id, s_i_id))
+FROM stock
+JOIN (
+    SELECT DISTINCT ol_w_id, ol_i_id
+    FROM order_line
+    WHERE ol_w_id = 1 AND ol_d_id = 1 AND ol_o_id >= d.d_next_o_id - 10
+) ol ON s_w_id = ol.ol_w_id AND s_i_id = ol.ol_i_id
+WHERE s_quantity < 1000;
+
+
+
