d0bde4d38c5f04010e951100b39e71495840e069
Zach <>
2020-11-06 01:48:49 +0800

    stock comparison

--- cassandra/src/transactions/stock_level.py
+++ cassandra/src/transactions/stock_level.py
@@ -7,9 +7,9 @@ def stock_level(session, warehouse, district, threshold, limit):
                      'LIMIT {l}'
     cql_get_items = 'SELECT OL_I_ID FROM order_line ' \
                     'WHERE OL_W_ID = {w} AND OL_D_ID = {d} AND OL_O_ID IN {o}'
-    cql_get_stocks = 'SELECT S_QUANTITY FROM stock ' \
-                     'WHERE S_W_ID = {w} AND S_I_ID IN {i}'
+    cql_get_stocks = 'SELECT COUNT(*) FROM stock ' \
+                     'WHERE S_W_ID = {w} AND S_I_ID IN {i} AND S_QUANTITY < {t}'
     orders = [order[0] for order in do_query(session, cql_get_orders.format(w=warehouse, d=district, l=limit))]
     items = set([item[0] for item in do_query(session, cql_get_items.format(w=warehouse, d=district, o=get_tuple(orders)))])
-    stocks = [1 for stock in do_query(session, cql_get_stocks.format(w=warehouse, i=get_tuple(items))) if int(stock[0]) < int(threshold)]
-    return len(stocks)
+    row = do_query(session, cql_get_stocks.format(w=warehouse, i=get_tuple(items), t=threshold))
+    return row[0].count
