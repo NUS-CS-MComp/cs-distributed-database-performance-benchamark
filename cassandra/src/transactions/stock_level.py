f3d0afad4895d444ec506f2be382857ca4670326
cs4224o <cs4224o@xcnc35.comp.nus.edu.sg>
2020-11-06 02:57:19 +0800

    revert change

--- cassandra/src/transactions/stock_level.py
+++ cassandra/src/transactions/stock_level.py
@@ -8,7 +8,7 @@ def stock_level(session, warehouse, district, threshold, limit):
     cql_get_items = 'SELECT OL_I_ID FROM order_line ' \
                     'WHERE OL_W_ID = {w} AND OL_D_ID = {d} AND OL_O_ID IN {o}'
     cql_get_stocks = 'SELECT COUNT(*) FROM stock ' \
-                     'WHERE S_W_ID = {w} AND S_I_ID IN {i} AND S_QUANTITY < {t}'
+                     'WHERE S_W_ID = {w} AND S_I_ID IN {i} AND S_QUANTITY < {t} ALLOW FILTERING'
     orders = [order[0] for order in do_query(session, cql_get_orders.format(w=warehouse, d=district, l=limit))]
     items = set([item[0] for item in do_query(session, cql_get_items.format(w=warehouse, d=district, o=get_tuple(orders)))])
     row = do_query(session, cql_get_stocks.format(w=warehouse, i=get_tuple(items), t=threshold))
